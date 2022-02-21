1ec489330ae819a836d65ccc0e962ea39c99c662
VladimirMolchanov <molchanov.vladimir1993@gmail.com>
2022-02-21 22:31:55 +0300

    4. Reusable input field

--- src/layouts/login.jsx
+++ src/layouts/login.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import TextField from "../components/textField";
 
 const Login = () => {
-    return <h1>Login</h1>;
+    const [data, setData] = useState({
+        email: "",
+        password: ""
+    });
+    const handleChange = ({ target }) => {
+        setData((prevState) => ({
+            ...prevState,
+            [target.name]: target.value
+        }));
+    };
+    return (
+        <form action="">
+            <div>
+                <TextField
+                    label="Электронная почта"
+                    name="email"
+                    value={data.email}
+                    onChange={handleChange}
+                />
+            </div>
+            <div>
+                <TextField
+                    label="Пароль"
+                    type="password"
+                    name="password"
+                    value={data.password}
+                    onChange={handleChange}
+                />
+            </div>
+        </form>
+    );
 };
 
 export default Login;
