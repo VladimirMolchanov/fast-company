23b762dbaf9d5c7bae74f635de28c640ed5b179f
VladimirMolchanov <molchanov.vladimir1993@gmail.com>
2022-04-02 16:49:24 +0300

    homework 3

--- src/components/ui/comments.jsx
+++ src/components/ui/comments.jsx
@@ -1,21 +1,45 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { orderBy } from "lodash";
 import AddCommentForm from "../common/comments/addCommentForm";
 import CommentsList from "../common/comments/commentsList";
-import { useComment } from "../../hooks/useComments";
+import { useDispatch, useSelector } from "react-redux";
+import {
+    createComments,
+    getComments,
+    getCommentsLoadingStatus,
+    loadCommentsList,
+    removeComment
+} from "../../store/comments";
+import { useParams } from "react-router-dom";
+import { nanoid } from "nanoid";
+import { getCurrentUserId } from "../../store/users";
+
 const Comments = () => {
-    const { createComment, comments, removeComment } = useComment();
+    const { userId } = useParams();
+    const dispatch = useDispatch();
+    const isLoading = useSelector(getCommentsLoadingStatus());
+    useEffect(() => {
+        dispatch(loadCommentsList(userId));
+    }, [userId]);
+    const comments = useSelector(getComments());
+    const currentUserId = useSelector(getCurrentUserId());
     const handleSubmit = (data) => {
-        createComment(data);
+        const newData = {
+            ...data,
+            pageId: userId,
+            created_at: Date.now(),
+            userId: currentUserId,
+            _id: nanoid()
+        };
+        dispatch(createComments(newData));
     };
     const handleRemoveComment = (id) => {
-        removeComment(id);
+        dispatch(removeComment(id));
     };
     const sortedComments = orderBy(comments, ["created_at"], ["desc"]);
     return (
         <>
             <div className="card mb-2">
-                {" "}
                 <div className="card-body ">
                     <AddCommentForm onSubmit={handleSubmit} />
                 </div>
@@ -29,7 +53,11 @@ const Comments = () => {
                             <div className="card-body ">
                                 <h2>Comments</h2>
                                 <hr />
-                                <CommentsList comments={sortedComments} onRemove={handleRemoveComment} />
+                                {!isLoading ? (
+                                    <CommentsList comments={sortedComments} onRemove={handleRemoveComment} />
+                                ) : (
+                                    "Loading"
+                                )}
                             </div>
                         </div>
                     )}
