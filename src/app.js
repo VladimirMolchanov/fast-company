b055d9539c76ff004b22a5ed5c226d4e5163ff72
VladimirMolchanov <molchanov.vladimir1993@gmail.com>
2022-04-02 16:53:28 +0300

    delete hooks

--- src/app.js
+++ src/app.js
@@ -5,7 +5,6 @@ import Login from "./layouts/login";
 import users from "./layouts/users";
 import NavBar from "./components/ui/navBar";
 import { ToastContainer } from "react-toastify";
-import AuthProvider from "./hooks/useAuth";
 import ProtectedRoute from "./components/common/protectedRoute";
 import LogOut from "./layouts/logOut";
 import AppLoader from "./components/ui/hoc/app.Loader";
@@ -14,17 +13,15 @@ export const App = () => {
     return (
         <>
             <AppLoader>
-                <AuthProvider>
-                    <NavBar />
-                    <Switch>
-                        <ProtectedRoute path="/users/:userId?/:edit?" component={users} />
-                        <Route path="/login/:type?" component={Login} />
-                        <Route path="/logout" component={LogOut} />
-                        <Route path="/" exact component={Main} />
-                        <Redirect to="/" />
-                    </Switch>
-                    <ToastContainer />
-                </AuthProvider>
+                <NavBar />
+                <Switch>
+                    <ProtectedRoute path="/users/:userId?/:edit?" component={users} />
+                    <Route path="/login/:type?" component={Login} />
+                    <Route path="/logout" component={LogOut} />
+                    <Route path="/" exact component={Main} />
+                    <Redirect to="/" />
+                </Switch>
+                <ToastContainer />
             </AppLoader>
         </>
     );
